5a88186fbbfe45d4f61a6c43a349b6fc98a9dd4b
liyangkun <yangkun1127@126.com>
2017-07-25 18:48:42 +0800

    上传import基本引入内容
    Signed-off-by: liyangkun <yangkun1127@126.com>

---
+++ import.js
@@ -0,0 +1,51 @@
+// ant-design配合dva常用插件
+
+// 模块导出时采用：
+// export default module;  引入是不需要{}
+// export module;  引入是需要{}
+
+import { routerRedux } from 'dva/router'   //路由监控模块
+//用于项目内页面跳转，查看详情页等操作
+dispatch(routerRedux.push({
+        pathname: '/home',  //跳转地址
+        query: {}           //跳转传递参数
+}))
+
+import Cookie from 'js-cookie'   //获取设置cookie的方法
+//用于对cookie的操作，存储用户信息等
+Cookie.set("userId",data.result.userId); //设置用户Id
+const id = Cookie.get("userId")   //读取用户Id  
+
+import { parse } from 'qs'   //json格式转换方法
+//发送请求是数据格式化
+const data = yield call(query,parse(payload))
+
+import React, { PropTypes } from 'react' //引入react和规定类型模块
+//引入React
+class Index extends React.Component{
+	  render(){
+	  	 return (
+	  	 )
+	  }
+}
+//限制组件传递值时候的类型
+Index.propTypes = {
+	loading : PropTypes.any,
+	dataSource : PropTypes.array,
+	onDelete : PropTypes.func,
+}
+//共有六种限制
+PropTypes.array
+PropTypes.bool
+PropTypes.func
+PropTypes.number
+PropTypes.object
+PropTypes.string
+
+
+import { connect } from 'dva'  //用于route和model的连接
+//用于连接
+function mapStateToProps({ model的namespace }) {
+  return { model的namespace }
+}
+export default connect(mapStateToProps)(组件名称)
